4ec9f57c25275ddbf0b6036bd8d31f3ea9c03bb9
cfzimmerman <coryzimmerman93@gmail.com>
2024-05-11 14:20:09 -0400

    remove freertos plus

--- FreeRTOS-Plus/Demo/rem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p deleted
-
-for d in */; do
-    if [[ $d != *"FreeRTOS_Plus_TCP_Echo_Qemu_mps2"* && $d != *"Common"* && $d != *"deleted"* ]]; then
-        mv "$d" "deleted/$d"
-    fi
-done
